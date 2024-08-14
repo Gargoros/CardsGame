eeb0463e32eb5a53e0a6aea8ef33ffdb0e1ea18d
Gargoros <mikhailzhachko@gmail.com>
2024-08-14 17:57:44 +0300

    init
    create new project

---
+++ CardsGame/CardsGame/App/CardsGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsGameApp.swift
+//  CardsGame
+//
+//  Created by MIKHAIL ZHACHKO on 14.08.24.
+//
+
+import SwiftUI
+
+@main
+struct CardsGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
